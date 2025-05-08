6d3bd7ce8cc2c296b2207d58c9eda6615db15da2
XueJia Wang <wxjwangxuejia@outlook.com>
2025-05-08 22:08:49 +0800

    feat: add _20_SwapChainRecreation
    Signed-off-by: XueJia Wang <wxjwangxuejia@outlook.com>

---
+++ _20_SwapChainRecreation/Vulkan/QueueFamily/QueueFamilyIndices.cpp
@@ -0,0 +1,18 @@
+#include "QueueFamilyIndices.h"
+
+bool QueueFamilyIndices::isComplete()
+{
+    return graphicsFamily.has_value() && presentFamily.has_value();
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ _20_SwapChainRecreation/Vulkan/QueueFamily/QueueFamilyMgr.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "QueueFamilyIndices.h"
+#include "../PhysicalDevicesMgr.h"
+
+class QueueFamilyMgr
+{
+public:
+    static QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
